93d16b1df0d0730bdadcfb7607170e9a135b8adc
vladar21 <vladar21@gmail.com>
2021-03-05 00:08:19 +0200

    6

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -5,6 +5,8 @@ const handlers = require('./lib/handlers')
 
 const app = express()
 
+app.disable('x-powered-by')
+
 // Настройка механизма представлений Handlebars.
 app.engine(
 	'handlebars',
@@ -20,6 +22,13 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', handlers.home)
 app.get('/about', handlers.about)
+app.get('/headers', (req, res) => {
+  res.type('text/plain')
+  const headers = Object.entries(req.headers)
+    .map(([key, value]) => `${key}: ${value}`)
+  res.send(headers.join('\n'))
+})
+
 // Пользовательская страница 404
 app.use(handlers.notFound)
 // Пользовательская страница 500
@@ -28,7 +37,7 @@ app.use(handlers.serverError)
 if(require.main === module) {
 app.listen(port, () => {
 console.log( `Express запущен на http://localhost:${port}` +
-'; нажмите Ctrl+C для завершения.' )
+`; нажмите Ctrl+C для завершения.` )
 })
 } else {
 module.exports = app
